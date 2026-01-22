a5d375720f220193e53aa5bf4d81f6a5b7453edb
Scoopote <Louis.Coulongeon2106@gmail.com>
2026-01-22 02:19:41 +0100

    Fix formatting in login script by adding missing comma in headers

--- FrontEnd/assets/js/loginScript.js
+++ FrontEnd/assets/js/loginScript.js
@@ -16,7 +16,8 @@ async function submitForm(event) {
     await fetch('http://localhost:5678/api/users/login',{
     method:'POST',
     headers:{
-        'Content-Type':'application/json'
+        'Content-Type':'application/json',
+        
     },
     body: JSON.stringify(body)
     }).then(response =>{
